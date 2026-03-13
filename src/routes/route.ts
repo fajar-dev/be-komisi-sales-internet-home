d9ad34051adcec6416e6b361a4b9363cc931a909
fajar-dev <fajarrivaldi2015@gmail.com>
2026-03-13 14:27:26 +0700

    fix: middleware

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -27,9 +27,9 @@ route.get('/additional/invoice', (c) => new AdditionalController().getInvoice(c)
 route.get('/employee/:id', authMiddleware, (c) => new EmployeeController().getEmployeeByEmployeeId(c));
 route.get('/employee/:id/hierarchy', authMiddleware, (c) => new EmployeeController().getEmployeeHierarchy(c));
 
-route.get('/sales/:id/commission', (c) => new CommissionController().salesCommission(c));
-route.get('/sales/:id/commission/period', (c) => new CommissionController().salesCommissionPeriod(c));
-route.get('/sales/:id/churn', (c) => new SnapshotController().salesChurn(c));
+route.get('/sales/:id/commission', authMiddleware, hierarchyMiddleware, (c) => new CommissionController().salesCommission(c));
+route.get('/sales/:id/commission/period', authMiddleware, hierarchyMiddleware, (c) => new CommissionController().salesCommissionPeriod(c));
+route.get('/sales/:id/churn', authMiddleware, hierarchyMiddleware, (c) => new SnapshotController().salesChurn(c));
 
 route.get('/manager/:id/commission', authMiddleware, hierarchyMiddleware, (c) => new CommissionController().managerCommission(c));
 route.get('/manager/:id/commission/period', authMiddleware, hierarchyMiddleware, (c) => new CommissionController().managerCommissionPeriod(c));
